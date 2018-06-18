7bd8e9d52d24cbb9236c9856ce8f7b4314607984
Varun Upadhyay <varun.u28@gmail.com>
2018-06-18 10:32:11 -0700

    Added code for database calls

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,9 +1,29 @@
+import sqlite3 as lite
+
+
 def create_database(database_path: str):
-    # TODO: generate the database
-    pass
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        cur.execute("drop table if exists words")
+        ddl = "create table words (usage_count int default 1 not null, word text not null primary key);"
+        cur.execute(ddl)
+        ddl = "create unique index words_word_uindex on words (word);"
+        cur.execute(ddl)
 
 
 def save_words_to_database(database_path: str, word_list: list):
-    # TODO: save the words to database
-    pass
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        for word in word_list:
+            sql = "select count(word) from words where word = '" + word + "'"
+            cur.execute(sql)
+            count = cur.fetchone()[0]
+            if count > 0:
+                sql = "update words set usage_count = usage_count + 1 where word = '" + word + "'"
+            else:
+                sql = "insert into words(word) values ('" + word + "')"
 
+            cur.execute(sql)
+        print("Database save complete")
